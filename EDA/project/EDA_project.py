f8755a85180383eb0c461093d9084404da284684
Panjiiiiiii <110644169+Panjiiiiiii@users.noreply.github.com>
2024-07-12 05:56:22 +0700

    Done explanatory data analysis

--- EDA/project/EDA_project.py
+++ EDA/project/EDA_project.py
@@ -15,7 +15,9 @@ product_df.head()
 sales_df = pd.read_csv("https://raw.githubusercontent.com/dicodingacademy/dicoding_dataset/main/DicodingCollection/sales.csv")
 sales_df.head()
 
+#EDA DATA CUSTOMER AND ORDERS
 customers_df.describe(include="all")
+orders_df.describe(include="all")
 
 #EDA group by gender
 group_by_gender = customers_df.groupby(by="gender").agg({
@@ -67,3 +69,81 @@ group_by_gender = order_customer_df.groupby(by="gender").order_id.nunique().sort
 order_customer_df["age_group"] = order_customer_df.age.apply(lambda x: "Youth" if x <= 24 else ("Seniors" if x > 64 else "Adults"))
 group_by_age = order_customer_df.groupby(by="age_group").order_id.nunique().sort_values(ascending=False)
 # print(group_by_age)
+
+#EDA DATA PRODUCT AND SALES
+product_df.describe(include='all')
+sales_df.describe(include='all')
+
+#EDA sort price product
+price_product = product_df.sort_values(by="price", ascending=False)
+# print(price_product)
+
+#EDA product and type
+product_type = product_df.groupby(by="product_type").agg({
+    "product_id" : "nunique",
+    "quantity" : "sum",
+    "price" : ["min", "max"]
+})
+
+product_name = product_df.groupby(by="product_name").agg({
+    "product_id" : "nunique",
+    "quantity" : "sum",
+    "price" : ["min", "max"]
+})
+# print(product_type)
+# print(product_name)
+
+#EDA best selling product
+sales_product_df = pd.merge(
+    left=sales_df,
+    right=product_df,
+    how="left",
+    left_on="product_id",
+    right_on="product_id"
+)
+# print(sales_product_df.head())
+
+#EDA sales basic on product
+sales_product_type = sales_product_df.groupby(by="product_type").agg({
+    'sales_id' : 'nunique',
+    "quantity_x" : "sum",
+    "total_price" : "sum"
+})
+# print(sales_product_type)
+sales_product_name = sales_product_df.groupby(by="product_name").agg({
+    'sales_id' : 'nunique',
+    "quantity_x" : "sum",
+    "total_price" : "sum"
+}).sort_values(by="total_price", ascending=False).reset_index().head(6)
+# print(sales_product_name)
+
+#EDA all data
+all_df = pd.merge(
+    left=sales_product_df,
+    right=order_customer_df,
+    how="left",
+    left_on="order_id",
+    right_on="order_id"
+)
+# print(all_df.head())
+
+#EDA preference by state
+preference_state = all_df.groupby(by=["state", "product_type"]).agg({
+    "quantity_x" : "sum",
+    "total_price" : "sum"
+})
+# print(preference_state)
+
+#EDA preference by gender
+preference_gender = all_df.groupby(by=["gender", "product_type"]).agg({
+    "quantity_x" : "sum",
+    "total_price" : "sum"
+})
+# print(preference_gender)
+
+#EDA preference by age
+preference_age = all_df.groupby(by=["age_group", "product_type"]).agg({
+    "quantity_x" : "sum",
+    "total_price" : "sum"
+})
+# print(preference_age)
